a4103fa4c42c81821e858e4ceca360889eb3e98a
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 20:34:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,11 @@
 
 def line(line)
   message = "The line is currently empty.";
-  line.each do |n|
-
+  line.each_with_index do |n, i|
+      if i = 0
+        message = "The line is currently: "
+      end
+      message << (i + 1).to_s + n
   end
   puts message
 end
