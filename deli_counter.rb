51e5ad25915763202bcb5ec71e1ce04852861405
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 20:24:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,34 +1,18 @@
 
-function takeANumber(line, name){
-  var message;
-  line.push(name);
-  message = `Welcome, ${name}. You are number ${line.length} in line.`
-  return message;
-}
 
-function nowServing(line){
-  var message;
-  if (line.length > 0){
-    message = "Currently serving " + line[0] + ".";
-    line.shift();
-  } else {
-    message = "There is nobody waiting to be served!"
-  }
-  return message
-}
-
-function currentLine(line){
-  var message;
-  if (line.length > 0){
+def currentLine(line)
+  message = "";
+  i = 0
+  if line.length > 0
     message = "The line is currently: ";
-    for (let i = 0; i < line.length; i++){
+    while i < line.length
       message = message + (i + 1) + ". " + line[i];
-      if (i !== line.length - 1){
+      if i !== line.length - 1
          message = message + ", "
-      }
-    }
-  } else {
+      end
+    end
+  else
     message = "The line is currently empty."
-  }
+  end
   return message
 }
