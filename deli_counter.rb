c15f2c0a8a14f793eaf5a8a452bd9f83fd058a9d
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 20:47:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,9 @@ require "pry"
 def line(line)
   message = "The line is currently empty.";
   line.each_with_index do |n, index|
-    puts "test"
-      #if index = 0
-      #  message = "The line is currently: "
-    #  end
+    if index = 0
+      message = "The line is currently: "
+    end
       #puts index
       #message << (index + 1).to_s + ". " + n
       #binding.pry
