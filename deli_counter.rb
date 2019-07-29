9e73a8fa36084beb0e74cb72b65f470dd8059afd
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 20:49:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(line)
   line.each_with_index do |n, index|
     if index = 0
       message = "The line is currently: "
+      binding.pry
     end
     message = message + (index + 1).to_s + ". " + n
       #binding.pry
