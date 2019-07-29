d72d0be03be7cacfc99a7cf8384c884afb353469
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 21:04:02 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+require "pry"
+
+def line(line)
+  message = "The line is currently empty.";
+  line.each_with_index do |n, index|
+    if index == 0
+      message = "The line is currently: "
+    end
+    message << (index + 1).to_s
+    message << ". "
+    message << n
+    if index < line.size - 1
+      message << " "
+    end
+
+  end
+  puts message
+end
+
+def take_a_number(line, name)
+  line.push(name);
+  message = "Welcome, #{name}. You are number #{line.length} in line."
+  puts message
+end
+
+def now_serving(line)
+  if line.length > 0
+    message = "Currently serving " + line[0] + "."
+    line.shift()
+  else
+    message = "There is nobody waiting to be served!"
+  end
+  puts message
+  return message
+end
